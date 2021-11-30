4371e8d5e71036d611b5335cfaca057424b63738
Hitesh Sukhwani <hiteshsukhwani29@gmail.com>
2021-11-30 17:28:00 +0530

    Create routes and schema for notes and users

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,10 @@ ConnecttoMongo();
 const app = express()
 const port = 3000
 
+app.use('/api/auth',require ('./routes/auth'))
+app.use('/api/notes',require ('./routes/notes'))
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })

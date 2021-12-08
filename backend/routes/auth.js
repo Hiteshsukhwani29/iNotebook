c95e203a7fb67c9d1dc4898eb8c812ad3cb46715
Hitesh Sukhwani <hiteshsukhwani29@gmail.com>
2021-12-09 01:27:06 +0530

    Create Login Endpoint

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -49,4 +49,46 @@ router.post('/createuser',[
     res.json({authtoken})
 })
 
+
+
+router.post('/login',[
+  // body('email','Enter a valid Email').isEmail(),
+  // body('password','Password cannot be Null').exists()
+],async (req,res)=>{
+
+  const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+  const {email,password} = req.body;
+  try{
+    let user = await User.findOne({email});
+    if(!user){
+      return res.status(400).json({ errors: "Invalid Credentials" });}
+
+      const comp = await bcrypt.compare(password,user.password);
+      if(!comp){
+        return res.status(400).json({ errors: "Invalid Pass" });
+      }
+
+      const data = {
+        user:{
+          id: user.id
+        }
+      }
+      const authtoken = jwt.sign(data, JWT_SECRET);
+      res.json({authtoken})
+
+    
+  }catch(error){
+
+  }
+
+
+
+})
+
+
+
 module.exports=router

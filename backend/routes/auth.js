02a62e0d0351913a122385f7381066878896227f
Hitesh Sukhwani <hiteshsukhwani29@gmail.com>
2021-12-08 23:41:18 +0530

    Add bcryptjs

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,6 +5,9 @@ const { body, validationResult } = require('express-validator');
 
 const User = require('../models/User');
 
+const bcrypt = require('bcrypt');
+var jwt = require('jsonwebtoken');
+
 // router.get('/',(req,res)=>{
 //     console.log(req.body);
 //     const user = User(req.body);
@@ -12,7 +15,8 @@ const User = require('../models/User');
 //     res.send(req.body);
 // })
 
-router.post('/',[
+
+router.post('/createuser',[
     body('email','Enter a valid Email').isEmail(),
     body('name','Enter a valid name').isLength({ min: 3 }),
     body('password','Password must be atleast 5 characters').isLength({ min: 5 })
@@ -23,15 +27,26 @@ router.post('/',[
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    let user = User.findOne({ email: req.body.email})
+    let user = await User.findOne({ email: req.body.email})
     if(user){
       res.status(400).json({ error : "User with same mail address already exists" });
     }
+    const salt = await bcrypt.genSalt(10);
+    const secPass = await bcrypt.hash(req.body.password, salt) 
+
     user = await User.create({
       name: req.body.name,
-      password: req.body.password,
+      password: secPass,
       email: req.body.email
-    }).then(user => res.json(user));
+    });
+    const data = {
+      user:{
+        id: user.id
+      }
+    }
+    const authtoken = jwt.sign(data, JWT_SECRET);
+
+    res.json({authtoken})
 })
 
 module.exports=router

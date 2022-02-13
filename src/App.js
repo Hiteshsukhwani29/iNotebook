2657c8cadacd05b1daff443e8909a6f196a1d1cf
Hitesh Sukhwani <hiteshsukhwani29@gmail.com>
2022-02-13 21:14:08 +0530

    create context api

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import About from './components/About';
+import Navbar from './components/navbar/Navbar';
+import Home from './components/home/Home';
+import About from './components/about/About';
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from 'react-router-dom';
+import NoteState from './context/notes/NoteState';
 
 function App() {
   return (
-    <Router>
-      <Navbar/>
+    <NoteState>
+      <Router>
+        <Navbar />
         <Switch>
           <Route exact path='/'>
-            <Home/>
+            <Home />
           </Route>
           <Route path='/about'>
-            <About/>
+            <About />
           </Route>
         </Switch>
       </Router>
+    </NoteState>
   );
 }
 
---
+++ src/components/about/About.js
@@ -0,0 +1,15 @@
+import React,{ useContext,useEffect } from 'react';
+import NoteContext from '../../context/notes/NoteContext';
+
+
+function About() {
+
+  //const context = useContext(NoteContext);
+
+
+  return (
+    <div>About</div>
+  )
+}
+
+export default About

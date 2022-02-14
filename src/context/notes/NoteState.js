4680b8fd5b1e7d8b1f19f303e71c9f1012a3509d
Hitesh Sukhwani <hiteshsukhwani29@gmail.com>
2022-02-15 01:40:00 +0530

    create edit note

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -3,73 +3,85 @@ import NoteContext from "./NoteContext";
 
 const NoteState = (props) => {
 
-    const n = [
-        {
-          "_id": "6208d7afb9471f1d7d837d89",
-          "user": "61ffa37b3fb039d6c1ef2185",
-          "title": "hhhhhhh",
-          "description": "hello world",
-          "tag": "general",
-          "__v": 0
-        },
-        {
-          "_id": "6208d7b8b9471f1d7d837d8e",
-          "user": "61ffa37b3fb039d6c1ef2185",
-          "title": "hhhhhhh",
-          "description": "hello world",
-          "tag": "general",
-          "__v": 0
-        },
-        {
-          "_id": "6208d7c5b9471f1d7d837d92",
-          "user": "61ffa37b3fb039d6c1ef2185",
-          "title": "hhhhhhh",
-          "description": "hello world",
-          "tag": "general",
-          "__v": 0
-        },
-        {
-          "_id": "6208d7ccb9471f1d7d837d94",
-          "user": "61ffa37b3fb039d6c1ef2185",
-          "title": "hhhhhsdfghh",
-          "description": "hello world",
-          "tag": "general",
-          "__v": 0
-        },
-        {
-          "_id": "6208d7cdb9471f1d7d837d96",
-          "user": "61ffa37b3fb039d6c1ef2185",
-          "title": "hhhhhsdfghh",
-          "description": "hello world",
-          "tag": "general",
-          "__v": 0
-        }
-      ]
+  const n = [
+    {
+      "_id": "6208d7afb9471f1d7d837d89",
+      "user": "61ffa37b3fb039d6c1ef2185",
+      "title": "hhhhhhh",
+      "description": "hello world",
+      "tag": "general",
+      "__v": 0
+    },
+    {
+      "_id": "6208d7b8b9471f1d7d837d8e",
+      "user": "61ffa37b3fb039d6c1ef2185",
+      "title": "hhhhhhh",
+      "description": "hello world",
+      "tag": "general",
+      "__v": 0
+    },
+    {
+      "_id": "6208d7c5b9471f1d7d837d92",
+      "user": "61ffa37b3fb039d6c1ef2185",
+      "title": "hhhhhhh",
+      "description": "hello world",
+      "tag": "general",
+      "__v": 0
+    },
+    {
+      "_id": "6208d7ccb9471f1d7d837d94",
+      "user": "61ffa37b3fb039d6c1ef2185",
+      "title": "hhhhhsdfghh",
+      "description": "hello world",
+      "tag": "general",
+      "__v": 0
+    },
+    {
+      "_id": "6208d7cdb9471f1d7d837d96",
+      "user": "61ffa37b3fb039d6c1ef2185",
+      "title": "hhhhhsdfghh",
+      "description": "hello world",
+      "tag": "general",
+      "__v": 0
+    }
+  ]
 
-      const [notes, setnotes] = useState(n);
+  const [notes, setnotes] = useState(n);
 
-      const addnote=(title,desc,tag)=>{
-        console.log(`working , Working${title}`);
-        const note = {
-          "_id": "6208d7cdb94213e2r4537d96",
-          "user": "61ffa37b3fb056781ef2185",
-          "title": title,
-          "description":desc,
-          "tag":tag
-        }
-        setnotes(notes.concat(note));
+  const addnote = (title, desc, tag) => {
+    console.log(`working , Working${title}`);
+    const note = {
+      "_id": "6208d7cdb94213e2r4537d96",
+      "user": "61ffa37b3fb056781ef2185",
+      "title": title,
+      "description": desc,
+      "tag": tag
+    }
+    setnotes(notes.concat(note));
+  }
+
+  const deletenote = (id) => {
+    const s = notes.filter((note) => { return note._id !== id });
+    setnotes(s);
+  }
+
+  const editnote = (id, title, desc, tag) => {
+    for (let i = 0; i < notes.length; i++) {
+      const e = notes[i];
+      if (e._id === id) {
+        e.title = title;
+        e.description = desc;
+        e.tag = tag;
       }
 
-      const deletenote=(id)=>{
-        const s=notes.filter((note)=>{return note._id!==id});
-        setnotes(s);
-      }
+    }
+  }
 
-    return (
-        <NoteContext.Provider value={{notes,addnote,deletenote}}>
-            {props.children};
-        </NoteContext.Provider>
-    )
+  return (
+    <NoteContext.Provider value={{ notes, addnote, deletenote }}>
+      {props.children};
+    </NoteContext.Provider>
+  )
 }
 
 export default NoteState;
